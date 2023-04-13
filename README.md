5c8cd1dd7eaabc13e5cee773473b38e87c339176
Juan Martín Monasterio <jmonasterio.laba@solvd.com>
2023-04-13 17:06:54 -0300

    reducer to utils

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# react-laba-international-1-jmon-20-react-hooks
+# [Homework #20] - React API
+
+This homework was done using Vite with React. Various complex hooks from React such as `useReducer` and `useContext` are featured.
+
+## Scripts
+
+### Running on local
+
+To run it on localhost, first run `yarn` to install dependencies and then you can either run `yarn dev` for the development branch or you can build the app with `yarn build` and preview your production build with `yarn preview`.
+
+## Domain
+
+This is the [Github pages](https://solvd-jmonasterio.github.io/react-laba-international-1-jmon-20-react-hooks/) deployment. I manually copied the files on a new [repo](https://github.com/solvd-jmonasterio/react-laba-international-1-jmon-20-react-hooks/) since I wouldn't know how to script it.
---
+++ src/components/utils/reducer.js
@@ -0,0 +1,140 @@
+import {ACTIONS} from '../utils/constants';
+
+export const initialState = {
+  overwrite: false,
+  previousOperand: '',
+  currentOperand: '0',
+};
+
+export default function reducer(state, {type, payload}) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: payload.digit,
+        };
+      }
+      if (
+        payload.digit === '0' &&
+        state.currentOperand === initialState.currentOperand
+      ) {
+        return state;
+      }
+      if (payload.digit === '.' && state.currentOperand.includes('.')) {
+        return state;
+      }
+      if (
+        state.currentOperand === initialState.currentOperand &&
+        payload.digit !== '.'
+      ) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+        };
+      }
+
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
+      };
+
+    case ACTIONS.CALCULATE_PERCENT:
+      if (state.currentOperand === initialState.currentOperand) return state;
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: initialState.previousOperand,
+        currentOperand: state.currentOperand / 100,
+      };
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (
+        state.currentOperand == initialState.currentOperand &&
+        state.previousOperand == initialState.previousOperand
+      ) {
+        return state;
+      }
+      if (state.previousOperand == initialState.previousOperand) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: initialState.currentOperand,
+        };
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: initialState.currentOperand,
+      };
+
+    case ACTIONS.CLEAR:
+      return initialState;
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return initialState;
+      }
+      if (!state.currentOperand) {
+        return state;
+      }
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: initialState.currentOperand,
+        };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation === initialState.operation ||
+        state.currentOperand === initialState.currentOperand ||
+        state.previousOperand === initialState.previousOperand
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: initialState.previousOperand,
+        currentOperand: evaluate(state),
+        operation: null,
+      };
+  }
+}
+
+function evaluate({currentOperand, previousOperand, operation}) {
+  const previous = Number(previousOperand);
+  const current = Number(currentOperand);
+
+  let op = 0;
+
+  switch (operation) {
+    case '÷':
+      op = previous / current;
+      break;
+    case '×':
+      op = previous * current;
+      break;
+    case '-':
+      op = previous - current;
+      break;
+    case '+':
+      op = previous + current;
+      break;
+    default:
+      break;
+  }
+
+  return String(op);
+}
